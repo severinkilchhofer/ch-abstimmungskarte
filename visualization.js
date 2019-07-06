ab2c278d58ef2f2ea3d4aaa0a2c33e79e0cb6342
Severin Kilchhofer <>
2019-07-06 16:13:53 +0200

    added tooltip

--- visualization.js
+++ visualization.js
@@ -21,6 +21,13 @@ document.addEventListener("DOMContentLoaded", () => {
                         .parallels([40, 50])
                         .scale(12500);
 
+                    const tooltip = container.append('div')
+                        .style('opacity' , 0)
+                        .style('position' , 'fixed')
+                        .style('background' , 'rgba(255, 255, 255, 0.8)')
+                        .style('padding' , '0.5rem')
+                        .style('pointer-events' , 'none');
+
                     const pathGenerator = d3.geoPath().projection(projection);
 
                     // TODO: use other color range
@@ -39,6 +46,20 @@ document.addEventListener("DOMContentLoaded", () => {
                         .attr('fill', function (d) {
                             const yes = yesVotes.find(yesVote => yesVote.id == d.properties.id);
                             return colorScale(yes.ja_anteil);
+                        })
+                        .on('mouseenter', function (d) {
+                            tooltip
+                                .style('opacity', 1)
+                                .html(d.properties.name)
+                        })
+                        .on('mousemove', function (d) {
+                            tooltip
+                                .style('left', d3.event.pageX + 'px')
+                                .style('top', d3.event.pageY + 'px')
+                        })
+                        .on('mouseleave', function () {
+                            tooltip
+                                .style('opacity', 0)
                         })
                 })
         })
